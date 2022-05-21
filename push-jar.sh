9204bd656924d5171e0a4a7212ee5246eb80cf84
subham <subhammandal01@gmail.com>
2022-05-21 19:02:39 +0000

    updated config

--- push-jar.sh
+++ push-jar.sh
@@ -2,6 +2,6 @@
 
 export NEXUS_USER=$NEXUS_USER
 export NEXUS_PASS=$NEXUS_PASS
-export NEXUS_REPO=$NEXUS_REPO
+export NEXUS_REPO=snapshot
 
 mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy
